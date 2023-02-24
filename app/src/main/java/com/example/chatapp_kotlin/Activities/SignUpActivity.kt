40f44cab66d18870219e6aff2dd30b8eceba0c92
venkatesh-jacke <venkateshjacke@gmail.com>
2023-02-24 20:20:14 +0530

    Permissions checked

--- app/src/main/java/com/example/chatapp_kotlin/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp_kotlin/Activities/SignUpActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chatapp_kotlin.Activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +12,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.chatapp_kotlin.DataClass.User
 import com.example.chatapp_kotlin.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -30,6 +34,10 @@ class SignUpActivity : AppCompatActivity() {
     lateinit var mAuth: FirebaseAuth
     lateinit var mDatabase: FirebaseDatabase
 
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE =
+            123  // Request code for READ_EXTERNAL_STORAGE. It can be any number > 0.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,13 +50,25 @@ class SignUpActivity : AppCompatActivity() {
         etPassword = binding.etPassword
         btnSubmit = binding.btnSubmit
         tvLogin = binding.tvLogin
-        mAuth=FirebaseAuth.getInstance()
-        mDatabase= FirebaseDatabase.getInstance()
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance()
         val currentUser = mAuth.currentUser
         // If user is already logged in, redirect to FriendsActivity
-        if(currentUser != null){
+        if (currentUser != null) {
             startActivity(Intent(this, FriendsActivity::class.java))
             finish()
+        }
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )  // Check if the READ_EXTERNAL_STORAGE permission is already available.
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+            )
         }
 
 
@@ -67,11 +87,16 @@ class SignUpActivity : AppCompatActivity() {
         }
         btnSubmit.setOnClickListener {
             if (isSigningUp) {
-                if (isValid(etUserName.text.toString(), etEmail.text.toString(), etPassword.text.toString())) {
+                if (isValid(
+                        etUserName.text.toString(),
+                        etEmail.text.toString(),
+                        etPassword.text.toString()
+                    )
+                ) {
                     signUp()
                 }
             } else {
-                if (isValid(null,etEmail.text.toString(), etPassword.text.toString())) {
+                if (isValid(null, etEmail.text.toString(), etPassword.text.toString())) {
                     logIn()
                 }
             }
@@ -80,7 +105,7 @@ class SignUpActivity : AppCompatActivity() {
     }
 
 
-      // Function to check if the entered details are valid
+    // Function to check if the entered details are valid
     fun isValid(username: String?, email: String, password: String): Boolean {
         var isValid = true
 
@@ -90,10 +115,10 @@ class SignUpActivity : AppCompatActivity() {
 //                isValid = false
 //            }
 //        }
-          if(username==null){
-              etUserName.error="UserName cannot be empty"
-                isValid=false
-          }
+        if (username == null) {
+            etUserName.error = "UserName cannot be empty"
+            isValid = false
+        }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             etEmail.error = "Invalid email address"
@@ -104,70 +129,71 @@ class SignUpActivity : AppCompatActivity() {
 //            etPassword.error = "Password must contain at least 8 characters including 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character"
 //            isValid = false
 //        }
-          if(password.length<5){
-              etPassword.error="Password must be at least 6 characters"
-              isValid=false
-          }
+        if (password.length < 5) {
+            etPassword.error = "Password must be at least 6 characters"
+            isValid = false
+        }
 
         return isValid
     }
 
     // Function to create a new user
-        private fun signUp() {
+    private fun signUp() {
 
-            mAuth
-                .createUserWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
-                .addOnCompleteListener {
+        mAuth
+            .createUserWithEmailAndPassword(etEmail.text.toString(), etPassword.text.toString())
+            .addOnCompleteListener {
 
-                    if (it.isSuccessful) {
-                        // Add user to database
-                        mDatabase.getReference("users/"+mAuth.uid).setValue(
-                            User(etUserName.text.toString(),etEmail.text.toString(),"")
-                        )
+                if (it.isSuccessful) {
+                    // Add user to database
+                    mDatabase.getReference("users/" + mAuth.uid).setValue(
+                        User(etUserName.text.toString(), etEmail.text.toString(), "")
+                    )
 
-                        // Redirect to FriendsActivity
-                        Intent(this, FriendsActivity::class.java).also {
-                            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            startActivity(it)
-                        }
-                        Toast.makeText(this, "User Created", Toast.LENGTH_SHORT).show()
-                        Log.d("SignUpActivity", "User Created")
-
-                    } else {
-                        Toast.makeText(this, "User Creation Failed", Toast.LENGTH_SHORT).show()
-                        Log.d(
-                            "SignUpActivity",
-                            "Creation Failed due to: " + it.exception?.message.toString()
-                        )
+                    // Redirect to FriendsActivity
+                    Intent(this, FriendsActivity::class.java).also {
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
                     }
+                    Toast.makeText(this, "User Created", Toast.LENGTH_SHORT).show()
+                    Log.d("SignUpActivity", "User Created")
+
+                } else {
+                    Toast.makeText(this, "User Creation Failed", Toast.LENGTH_SHORT).show()
+                    Log.d(
+                        "SignUpActivity",
+                        "Creation Failed due to: " + it.exception?.message.toString()
+                    )
                 }
-        }
-  // Function to log in an existing user
-        private fun logIn() {
-
-            mAuth.signInWithEmailAndPassword(
-                binding.etEmail.text.toString(),
-                binding.etPassword.text.toString()
-            )
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-
-                        // Redirect to FriendsActivity
-                        Intent(this, FriendsActivity::class.java).also {
-                            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            startActivity(it)
-                        }
-                        Toast.makeText(this, "SignIn Successful", Toast.LENGTH_SHORT).show()
-                        Log.d("SignUpActivity", "SignIn Successful")
-
-                    } else {
-                        Toast.makeText(this, "SignIn Failed", Toast.LENGTH_SHORT).show()
-                        Log.d(
-                            "SignUpActivity",
-                            "SignIn Failed due to: " + it.exception?.message.toString()
-                        )
-                    }
-                }
-        }
-
+            }
     }
+
+    // Function to log in an existing user
+    private fun logIn() {
+
+        mAuth.signInWithEmailAndPassword(
+            binding.etEmail.text.toString(),
+            binding.etPassword.text.toString()
+        )
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+
+                    // Redirect to FriendsActivity
+                    Intent(this, FriendsActivity::class.java).also {
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
+                    }
+                    Toast.makeText(this, "SignIn Successful", Toast.LENGTH_SHORT).show()
+                    Log.d("SignUpActivity", "SignIn Successful")
+
+                } else {
+                    Toast.makeText(this, "SignIn Failed", Toast.LENGTH_SHORT).show()
+                    Log.d(
+                        "SignUpActivity",
+                        "SignIn Failed due to: " + it.exception?.message.toString()
+                    )
+                }
+            }
+    }
+
+}
